e5e155f6f7590e2ede948e36e187f00b1cc61d31
Martin Renters <martin@teckelworks.com>
2024-09-24 14:49:14 -0400

    Version 3.3.0

--- dftoolkit/__init__.py
+++ dftoolkit/__init__.py
@@ -18,7 +18,7 @@
 #
 '''DFtoolkit module'''
 
-__version__ = '3.2.0'
+__version__ = '3.3.0'
 __VERSION__ = __version__
 
 import sys
--- sigtrack/src/main.c
+++ sigtrack/src/main.c
@@ -29,7 +29,7 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-#define VERSION	"3.2.0"
+#define VERSION	"3.3.0"
 
 int yyparse(void);
 int get_config_error_cnt(void);
